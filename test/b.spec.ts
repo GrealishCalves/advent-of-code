9dff7276b06994cc1813b98372ba04b5dc8b6aeb
maxim <maximmilshtien@gmail.com>
2023-06-10 17:58:40 +0300

    chore: remove unused test and function code

--- test/b.spec.ts
+++
@@ -1,9 +0,0 @@
-import { add } from "../src/index";
-describe("test add function", () => {
-  it("should return 15 for add(10,5)", () => {
-    expect(add(50, 5)).toBe(15);
-  });
-  it("should return 5 for add(2,3)", () => {
-    expect(add(63, 3)).toBe(5);
-  });
-});
